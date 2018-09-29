4bfde9637472e18daadcdc47f91d86c16a8c6fcc
GotoCode <khalid.peer233@gmail.com>
2018-09-29 14:42:31 +0300

    Replace jQuery UI logic with Bootstrap for mobile

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 
 $("document").ready(function() {
-	
-	$(".accordion").accordion({
-		animate: 150,
-		active: false,
-		collapsible: true,
-		heightStyle: "content"
-	});
 
-	//$("#first-accordion").hide();
 	$("#second-accordion").hide();
 	$("#third-accordion").hide();
 
 	$("#btn-project-topics").on("click", function(e) {
+		$("#btn-project-topics").removeClass("active").addClass("active");
+		$("#btn-developer-roles").removeClass("active");
+		$("#btn-programming-languages").removeClass("active");
+
 		$("#first-accordion").show();
 		$("#second-accordion").hide();
 		$("#third-accordion").hide();
 	});
 
 	$("#btn-developer-roles").on("click", function(e) {
+		$("#btn-project-topics").removeClass("active");
+		$("#btn-developer-roles").removeClass("active").addClass("active");
+		$("#btn-programming-languages").removeClass("active");
+
 		$("#second-accordion").show();
 		$("#first-accordion").hide();
 		$("#third-accordion").hide();
 	});
 
 	$("#btn-programming-languages").on("click", function(e) {
+		$("#btn-project-topics").removeClass("active");
+		$("#btn-developer-roles").removeClass("active");
+		$("#btn-programming-languages").removeClass("active").addClass("active");
+
 		$("#third-accordion").show();
 		$("#first-accordion").hide();
 		$("#second-accordion").hide();
